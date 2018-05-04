c46e2a53523e33b160e598c88a4487a451e6478c
Eric W <ericew@gmail.com>
2018-05-04 13:02:38 -0400

    Update blacklist.sh
    DROP is already in ET, quoting $url for safety

--- blacklist.sh
+++ blacklist.sh
@@ -8,10 +8,10 @@ ipset_list='temporary-list'
 sudo /sbin/ipset -! destroy $ipset_list
 sudo /sbin/ipset create $ipset_list hash:net
 
-for url in 'https://www.spamhaus.org/drop/drop.txt' 'https://www.spamhaus.org/drop/edrop.txt' 'http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt' 'https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1';
+for url in 'https://www.spamhaus.org/drop/edrop.txt' 'http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt' 'https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1';
 do
  echo "Fetching and processing $url"
- curl $url | awk '/^[1-9]/ { print $1 }' | xargs -n1 sudo ipset -q add $ipset_list
+ curl "$url" | awk '/^[1-9]/ { print $1 }' | xargs -n1 sudo ipset -q add $ipset_list
 done
 
 sudo /sbin/ipset swap $ipset_list $real_list
